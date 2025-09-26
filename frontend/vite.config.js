f01b0510f319572d66f19d6bd7e3ebe88ff36e1c
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-09-26 12:22:43 +0200

    test deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ROLLAND_GRELLETY_Carole_14_wealthhealth_082025/', // <— IMPORTANT
   plugins: [react()],
 })
